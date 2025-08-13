dce3505fc0383ef93d2652128c682c6f8c6e3efd
kumar gandhi <kumargandhi2k1.com>
2025-08-13 18:28:18 +0530

    mongo

--- bend/index.js
+++ bend/index.js
@@ -3,11 +3,11 @@ let mongoose=require("mongoose");
 let cors=require("cors")
 const rt = require("./routes/rt");
 let bodyParser=require("body-parser")
-mongoose.connect("mongodb://127.0.0.1:27017/resultportal").then(()=>{
-     console.log("ok");
+mongoose.connect("mongodb+srv://kumargandhi2k1:Kumar123@cluster0.pzqxbyy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") .then(()=>{
+    console.log("ok")
 })
 let app=express()
-app.use(express.json())
+app.use(express.json()) 
 app.use(cors())
 app.use(bodyParser.urlencoded({"extended":true}))
 app.use("/pic",express.static("./photos"))
